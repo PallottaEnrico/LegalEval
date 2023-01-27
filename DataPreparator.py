08565afd32e0daa9acd988c15a7294447f418067
Enrico Pallotta <57596722+PallottaEnrico@users.noreply.github.com>
2023-01-27 10:56:57 +0100

    Update and rename NERutils.py to DataPreparator.py

--- DataPreparator.py
+++ DataPreparator.py
@@ -211,12 +211,16 @@ class NERDataMaker:
         else:
             return [_process_tokens_for_one_text(i+idx.start, tee) for i, tee in enumerate(tokens_with_encoded_entities)]
 
-    def as_hf_dataset(self, window_length, sl_window) -> Dataset:
+    def as_hf_dataset(self, window_length, sl_window, CRF = False) -> Dataset:
 
         def generate_input_ids_labels(examples):
-            # remember we need to add the start and end token id (they will have label -100)
-            input_ids = [[self.tokenizer.cls_token_id] + self.tokenizer.convert_tokens_to_ids(e)[:window_length-2]+ [self.tokenizer.sep_token_id] for e in examples["tokens"]]
-            labels = [[-100] + e[:window_length-2] + [-100] for e in examples['ner_tags']] 
+            # CRF does not allow -100 as token id, so we'll not add it.
+            if CRF:
+                input_ids = [tokenizer.convert_tokens_to_ids(e)[:window_length-2] for e in examples["tokens"]]
+                labels = [e[:window_length-2] for e in examples['ner_tags']]   
+            else:
+                input_ids = [[self.tokenizer.cls_token_id] + self.tokenizer.convert_tokens_to_ids(e)[:window_length-2]+ [self.tokenizer.sep_token_id] for e in examples["tokens"]]
+                labels = [[-100] + e[:window_length-2] + [-100] for e in examples['ner_tags']] 
 
             tokenized_inputs = {
                 'input_ids' : input_ids, 
@@ -253,145 +257,4 @@ class NERDataMaker:
         ds = Dataset.from_dict(data, features)
         tokenized_ds = ds.map(generate_input_ids_labels, batched=True)
         return tokenized_ds
-      
-      
-class SlidingWindowNERPipeline(TokenClassificationPipeline):
-    """Modified version of TokenClassificationPipeline that uses a sliding
-    window approach to fit long texts into the limited position embeddings of a
-    transformer.
-    """
-
-    def __init__(self, aggregation_strategy, window_length: Optional[int] = None,
-                 stride: Optional[int] = None, *args, **kwargs):
-        super(SlidingWindowNERPipeline, self).__init__(
-            *args, **kwargs)
-        self.window_length = window_length or self.tokenizer.model_max_length
-        if stride is None:
-            self.stride = self.window_length // 2
-        elif stride == 0:
-            self.stride = self.window_length
-        elif 0 < stride <= self.window_length:
-            self.stride = stride
-        else:
-            raise ValueError("`stride` must be a positive integer no greater "
-                             "than `window_length`")
-        if aggregation_strategy == 'simple':
-            self.aggregation_strategy = AggregationStrategy.SIMPLE
-        elif aggregation_strategy == 'first':
-            self.aggregation_strategy = AggregationStrategy.FIRST
-        elif aggregation_strategy == 'average':
-            self.aggregation_strategy = AggregationStrategy.AVERAGE
-        elif aggregation_strategy == 'max':
-            self.aggregation_strategy = AggregationStrategy.MAX
-        else:
-            self.aggregation_strategy = AggregationStrategy.NONE
-        self.ignore_labels=["O"]
-
-    def __call__(self, inputs: Union[str, List[str]], **kwargs):
-        """
-        Classify each token of the text(s) given as inputs.
-
-        Args:
-            inputs (:obj:`str` or :obj:`List[str]`):
-                One or several texts (or one list of texts) for token classification.
-
-        Return:
-            A list or a list of list of :obj:`dict`: Each result comes as a list of dictionaries (one for each token in
-            the corresponding input, or each entity if this pipeline was instantiated with an aggregation_strategy)
-            with the following keys:
-
-            - **word** (:obj:`str`) -- The token/word classified.
-            - **score** (:obj:`float`) -- The corresponding probability for :obj:`entity`.
-            - **entity** (:obj:`str`) -- The entity predicted for that token/word (it is named `entity_group` when
-              `aggregation_strategy` is not :obj:`"none"`.
-            - **index** (:obj:`int`, only present when ``aggregation_strategy="none"``) -- The index of the
-              corresponding token in the sentence.
-            - **start** (:obj:`int`, `optional`) -- The index of the start of the corresponding entity in the sentence.
-              Only exists if the offsets are available within the tokenizer
-            - **end** (:obj:`int`, `optional`) -- The index of the end of the corresponding entity in the sentence.
-              Only exists if the offsets are available within the tokenizer
-        """
-
-        _inputs, offset_mappings = self._args_parser(inputs, **kwargs)
-
-        answers = []
-        num_labels = self.model.num_labels
-
-        for i, sentence in enumerate(_inputs):
-
-            # Manage correct placement of the tensors
-            with self.device_placement():
-                tokens = self.tokenizer(
-                    sentence,
-                    padding=True,
-                    return_attention_mask=False,
-                    return_tensors=self.framework,
-                    return_special_tokens_mask=True,
-                    add_special_tokens=True,
-                    return_offsets_mapping=self.tokenizer.is_fast
-                )
-                if self.tokenizer.is_fast:
-                    offset_mapping = \
-                        tokens.pop("offset_mapping").cpu().numpy()[0]
-                elif offset_mappings:
-                    offset_mapping = offset_mappings[i]
-                else:
-                    offset_mapping = None
-
-                special_tokens_mask = \
-                    tokens.pop("special_tokens_mask").cpu().numpy()[0]
-
-                if self.framework == "tf":
-                    raise ValueError("SlidingWindowNERPipeline does not "
-                                     "support TensorFlow models.")
-                # Forward inference pass
-                with torch.no_grad():
-                    #tokens = self.ensure_tensor_on_device(**tokens)
-                    tokens.to(self.device)
-                    # Get logits (i.e. tag scores)
-                    entities = np.zeros(tokens['input_ids'].shape[1:] +
-                                        (num_labels,))
-                    writes = np.zeros(entities.shape)
-                    for start in range(
-                            0, tokens['input_ids'].shape[1] - 1,
-                            self.stride):
-                        end = start + self.window_length - 2
-
-                        window_input_ids = torch.cat([
-                            torch.tensor([[self.tokenizer.cls_token_id]]).to(self.device),
-                            tokens['input_ids'][:, start:end],
-                            torch.tensor([[self.tokenizer.sep_token_id]]).to(self.device)
-                        ], dim=1)
-                        window_logits = self.model(
-                            input_ids=window_input_ids)[0][0].cpu().numpy()
-                        entities[start:end] += window_logits[1:-1]
-                        writes[start:end] += 1
-                    # Old way for getting logits under PyTorch
-                    # entities = self.model(**tokens)[0][0].cpu().numpy()
-                    input_ids = tokens["input_ids"].cpu().numpy()[0]
-                    entities = entities / writes
-
-                    scores = np.exp(entities) / np.exp(entities).sum(
-                        -1, keepdims=True)
-                    pre_entities = self.gather_pre_entities(
-                        sentence, input_ids, scores, offset_mapping,
-                        special_tokens_mask, aggregation_strategy=self.aggregation_strategy)
-                    grouped_entities = self.aggregate(
-                        pre_entities, self.aggregation_strategy)
-                    if self.aggregation_strategy != AggregationStrategy.NONE:
-                    # Filter anything that is in self.ignore_labels
-                        entities = [
-                            entity
-                            for entity in grouped_entities
-                            if entity.get("entity", None) not in self.ignore_labels
-                            and entity.get("entity_group", None) not in
-                            self.ignore_labels
-                        ]
-                        answers.append(entities)
-                    else:
-                        answers.append(grouped_entities)
-
-        if len(answers) == 1:
-            return answers[0]
-        return answers
 
