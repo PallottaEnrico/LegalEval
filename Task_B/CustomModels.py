ac23b24dfc04f12acad25cb112d7ee91581541d5
Enrico Pallotta <57596722+PallottaEnrico@users.noreply.github.com>
2023-01-27 12:12:54 +0100

    Update CustomModels.py

--- Task_B/CustomModels.py
+++ Task_B/CustomModels.py
@@ -257,3 +257,98 @@ class CustomRobertaForTokenClassification(RobertaPreTrainedModel):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )  
+    
+    
+class CustomModelForTokenClassification(XLNetPreTrainedModel):
+    def __init__(self, config, custom_layers):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.custom_layers = custom_layers
+        self.transformer = XLNetModel(config)
+        
+        if self.custom_layers['LSTM'] == True:
+            self.num_layers = 1
+            self.lstm = nn.LSTM(config.hidden_size, config.hidden_size//2, num_layers = self.num_layers, bidirectional=True, dropout=0.5)
+        
+        if self.custom_layers['CRF'] == True:
+            self.crf = CRF(config.num_labels, batch_first=True)
+            self.crf.reset_parameters()
+        
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        mems: Optional[torch.Tensor] = None,
+        perm_mask: Optional[torch.Tensor] = None,
+        target_mapping: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        input_mask: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        use_mems: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        **kwargs,  # delete when `use_cache` is removed in XLNetModel
+    ) -> Union[Tuple, XLNetForTokenClassificationOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the multiple choice classification loss. Indices should be in `[0, ..., num_choices]`
+            where *num_choices* is the size of the second dimension of the input tensors. (see *input_ids* above)
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.transformer(
+            input_ids,
+            attention_mask=attention_mask,
+            mems=mems,
+            perm_mask=perm_mask,
+            target_mapping=target_mapping,
+            token_type_ids=token_type_ids,
+            input_mask=input_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_mems=use_mems,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+                
+        if self.custom_layers['LSTM'] == True:
+            # Forward propagate through LSTM
+            sequence_output, _ = self.lstm(sequence_output) 
+
+        logits = self.classifier(sequence_output)
+        loss = None
+        
+        if self.custom_layers['CRF'] == True:
+            if labels is not None:
+                loss = -self.crf(emissions=logits, tags=labels, mask=attention_mask.byte(), reduction='token_mean')
+                logits = self.crf.decode(logits)
+            else:
+                logits = self.crf.decode(logits)
+            logits = torch.Tensor(logits)
+        else:
+            if labels is not None:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return XLNetForTokenClassificationOutput(
+            loss=loss,
+            logits=logits,
+            mems=outputs.mems,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )  
