c3b4e25329b207e3675d53c3befbf3582e6937cf
yurinoviello <84674377+yurinoviello@users.noreply.github.com>
2023-01-29 17:31:12 +0100

    Update CustomModels.py

--- Task_B/CustomModels.py
+++ Task_B/CustomModels.py
@@ -5,93 +5,12 @@ from torch import nn
 from torch.nn import CrossEntropyLoss
 from transformers import XLNetModel, XLNetPreTrainedModel
 from transformers.models.xlnet.modeling_xlnet import XLNetForTokenClassificationOutput
-from transformers import AutoModel, AutoConfig, RobertaPreTrainedModel, RobertaModel, RobertaConfig, XLMRobertaModel
+from transformers import AutoModel, AutoConfig, RobertaPreTrainedModel, RobertaModel, RobertaConfig
 from transformers.modeling_outputs import TokenClassifierOutput
 
-      
-class CustomXLMRobertaForTokenClassification(XLMRobertaForTokenClassification):
-    def __init__(self, config):
-        super().__init__(config)
-        self.num_labels = config.num_labels
-
-        self.roberta = XLMRobertaModel(config, add_pooling_layer=False)
-        classifier_dropout = (
-            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
-        )
-        self.dropout = nn.Dropout(classifier_dropout)
-        
-        self.num_layers = 2
-        self.lstm = nn.LSTM(self.roberta.config.hidden_size, (self.config.hidden_size)//2, num_layers = self.num_layers, bidirectional=True, batch_first=True)
-        
-        self.classifier = nn.Linear(self.config.hidden_size, config.num_labels)
-        self.crf = CRF(config.num_labels, batch_first=True)
-        self.crf.reset_parameters()
-        # Initialize weights and apply final processing
-        self.post_init()
-        
-    def forward(
-        self,
-        input_ids: Optional[torch.LongTensor] = None,
-        attention_mask: Optional[torch.FloatTensor] = None,
-        token_type_ids: Optional[torch.LongTensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> Union[Tuple[torch.Tensor], TokenClassifierOutput]:
-        r"""
-        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
-            Labels for computing the token classification loss. Indices should be in `[0, ..., config.num_labels - 1]`.
-        """
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        outputs = self.roberta(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        sequence_output = outputs[0]
-        
-        # Forward propagate through LSTM
-        sequence_output, _ = self.lstm(sequence_output)
-        
-        sequence_output = self.dropout(sequence_output)
-        logits = self.classifier(sequence_output)
-        
-        loss = None
-
-        if labels is not None:
-            loss = -self.crf(emissions=logits, tags=labels, mask=attention_mask.byte(), reduction='token_mean')
-            logits = self.crf.decode(logits)
-            
-        else:
-            logits = self.crf.decode(logits)
-        logits = torch.Tensor(logits)
-            
-
-        if not return_dict:
-            output = (logits,) + outputs[2:]
-            return ((loss,) + output) if loss is not None else output
-
-        return TokenClassifierOutput(
-            loss=loss,
-            logits=logits,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-        )
-    
+   
 class CustomRobertaForTokenClassification(RobertaPreTrainedModel):
-    def __init__(self, config):
+    def __init__(self, config, custom_layers):
         super().__init__(config)
         self.num_labels = config.num_labels
 
@@ -101,12 +20,17 @@ class CustomRobertaForTokenClassification(RobertaPreTrainedModel):
         )
         self.dropout = nn.Dropout(classifier_dropout)
         
-        # self.num_layers = 2
-        # self.lstm = nn.LSTM(self.roberta.config.hidden_size, self.config.hidden_size // 2, num_layers = self.num_layers, bidirectional=True, batch_first=True)
+        self.num_LSTM = custom_layers['LSTM'] if 'LSTM' in custom_layers.keys() else 0
+        self.CRF = 'CRF' in custom_layers.keys() and custom_layers['CRF'] == True
+
+        if self.num_LSTM:
+            self.lstm = nn.LSTM(config.hidden_size, config.hidden_size//2, num_layers = self.num_LSTM, bidirectional=True, dropout=0.5)
+        
+        if self.CRF:
+            self.crf = CRF(config.num_labels, batch_first=True)
+            self.crf.reset_parameters()
         
         self.classifier = nn.Linear(self.config.hidden_size, config.num_labels)
-        self.crf = CRF(config.num_labels, batch_first=True)
-        self.crf.reset_parameters()
         # Initialize weights and apply final processing
         self.post_init()
         
@@ -143,23 +67,28 @@ class CustomRobertaForTokenClassification(RobertaPreTrainedModel):
 
         sequence_output = outputs[0]
         
-        # # Forward propagate through LSTM
-        # sequence_output, _ = self.lstm(sequence_output)
+        if self.num_LSTM:
+            # Forward propagate through LSTM
+            sequence_output, _ = self.lstm(sequence_output)
         
         sequence_output = self.dropout(sequence_output)
         logits = self.classifier(sequence_output)
         
         loss = None
 
-        if labels is not None:
-            
-            loss = -self.crf(emissions=logits, tags=labels, mask=attention_mask.byte(), reduction='token_mean')
-            logits = self.crf.decode(logits)
-            
-        else:
-            logits = self.crf.decode(logits)
-        logits = torch.Tensor(logits)
-            
+        if self.CRF:
+              if labels is not None:
+
+                  loss = -self.crf(emissions=logits, tags=labels, mask=attention_mask.byte(), reduction='token_mean')
+                  logits = self.crf.decode(logits)
+
+              else:
+                  logits = self.crf.decode(logits)
+              logits = torch.Tensor(logits)
+         else:
+               if labels is not None:
+                  loss_fct = CrossEntropyLoss()
+                  loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
 
         if not return_dict:
             output = (logits,) + outputs[2:]
